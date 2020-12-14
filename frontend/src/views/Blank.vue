0e4c08aee939ff340baba931c24f6215a3e4d09d
fexd12 <felipe.demelo@hotmail.com>
2020-12-14 18:56:43 -0300

    arrumando envio da imagem por base64

--- frontend/src/views/Blank.vue
+++ frontend/src/views/Blank.vue
@@ -1,64 +1,67 @@
 ﻿<template>
   <div className="container">
     <div class="row justify-content-center py-5">
-			<b-form-group label-cols-sm="2" label-size="sm">
-				<b-img id="preview" :src="url" alt="" />
+      <b-form-group label-cols-sm="2" label-size="sm">
+        <b-img style="padding-bottom: 50px" id="preview" :src="imagem" alt="" />
 
-				<span
-					v-if="!url"
-					class="placeholder"
-				>
-					Choose an Image
-				</span>
+        <span v-if="!imagem" class="placeholder"> Choose an Image </span>
 
-				<b-form-file
-					type="file"
-					accept="image/*"
-					id="file-input"
-					size="sm"
-					@change="upload_picture($event)"
-				/>
-
-			</b-form-group>
-
-			<b-form-group label-cols-sm="2" label-size="sm">
-				<b-button size="sm" @click="uploadImage()"> Enviar </b-button>
-			</b-form-group>
-      
+        <b-form-file
+          type="file"
+          accept="image/*"
+          id="file-input"
+          size="sm"
+          @change="upload_picture($event)"
+        />
+        <div class="botao">
+          <b-button size="sm" @click="uploadImage()"> Enviar </b-button>
+        </div>
+      </b-form-group>
     </div>
+
+		<div class="botao" v-if="response">
+			<span>Texto enviado</span>
+		</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Blank",
-  data:() => {
+  data: () => {
     return {
-			imagem: [],
-			url:''
+			imagem: null,
+			response:null
     };
   },
   methods: {
     uploadImage() {
-      const URL = "http://foobar.com/upload";
+      // const URL = "http://localhost:3002/upload/";
+      // let config = {
+      //   header: {
+      //     "Content-Type": "application/json",
+      //   },
+			// };
+			
+			// let data = {
+			// 	imagem : this.imagem
+			// };
 
-      let config = {
-        header: {
-          "Content-Type": "image/png",
-        },
-      };
-
-      this.$http.put(URL,this.imagem, config).then((response) => {
-        console.log("image upload response > ", response);
-      });
+			this.response = true
+			console.log(this.response)
+      // this.$http.post(URL, data, config).then((response) => {
+      //   console.log("image upload response > ", response);
+      // });
     },
     upload_picture(event) {
-			let data = {
-				name: event.target.files[0].name,
-				file : event.target.files[0]
-			}
-			this.imagem.push(data);
-			this.url = URL.createObjectURL(data.file)
+
+			let reader = new FileReader();
+
+			reader.addEventListener("load",()=>{
+				this.imagem = reader.result;
+			},false);
+
+			reader.readAsDataURL(event.target.files[0]);			
     },
   },
 };
@@ -68,8 +71,13 @@ export default {
 #preview {
   max-width: 100%;
   max-height: 500px;
-	display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
+}
+.botao {
+  border-radius: 6px solid #eee;
+  padding-top: 5px;
+  text-align: center;
 }
 </style>
