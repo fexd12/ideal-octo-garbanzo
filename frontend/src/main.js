8d8672f8df6c07afdbaef7bfe1b706fa5f3f926e
Felipe <felipe.demelo@hotmail.com>
2020-12-14 01:11:51 -0300

    adicionando upload imagem

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,13 @@ import router from "@/router";
 import Vue from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.min.css";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
--- frontend/src/views/Blank.vue
+++ frontend/src/views/Blank.vue
@@ -1,16 +1,75 @@
 ﻿<template>
   <div className="container">
     <div class="row justify-content-center py-5">
-      <h3>Blank</h3>
+			<b-form-group label-cols-sm="2" label-size="sm">
+				<b-img id="preview" :src="url" alt="" />
+
+				<span
+					v-if="!url"
+					class="placeholder"
+				>
+					Choose an Image
+				</span>
+
+				<b-form-file
+					type="file"
+					accept="image/*"
+					id="file-input"
+					size="sm"
+					@change="upload_picture($event)"
+				/>
+
+			</b-form-group>
+
+			<b-form-group label-cols-sm="2" label-size="sm">
+				<b-button size="sm" @click="uploadImage()"> Enviar </b-button>
+			</b-form-group>
+      
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Blank"
+  name: "Blank",
+  data:() => {
+    return {
+			imagem: [],
+			url:''
+    };
+  },
+  methods: {
+    uploadImage() {
+      const URL = "http://foobar.com/upload";
+
+      let config = {
+        header: {
+          "Content-Type": "image/png",
+        },
+      };
+
+      this.$http.put(URL,this.imagem, config).then((response) => {
+        console.log("image upload response > ", response);
+      });
+    },
+    upload_picture(event) {
+			let data = {
+				name: event.target.files[0].name,
+				file : event.target.files[0]
+			}
+			this.imagem.push(data);
+			this.url = URL.createObjectURL(data.file)
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style >
+#preview {
+  max-width: 100%;
+  max-height: 500px;
+	display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
